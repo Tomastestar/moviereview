2986d05d61952244a51661d4cff6ef166a638200
Tomas Olszowy <tomas_o_83@hotmail.com>
2017-11-27 11:54:41 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,23 @@
+let movieData = {
+  title: "Inception",
+  imgUrl: "https://static.rogerebert.com/uploads/movie/movie_poster/inception-2010/large_ziKvu3Th9l1wN2aIeVj5ElpBqFu.jpg",
+  review: "A very meta movie about dreams within dreams. And then there was a whole bunch of shooting, not really sure how that fit into the picture.",
+  actors: [
+    "Leonardo DiCaprio",
+    "Ellen Page",
+    "Ken Watanabe",
+    "Någon annan skådis"
+  ]
+};
+
+let Starwars = {
+  title: "Starwars",
+  imgUrl: "https://images-na.ssl-images-amazon.com/images/M/MV5BZDk2NmNhZDgtZDgzZS00NTRkLWFiYjUtNGMzZTYwNTFhYjFmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,634,1000_AL_.jpg",
+  review: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station while also attempting to rescue Princess Leia from the evil Darth Vader.",
+  actors: [
+    "Mark Hammil",
+    "Harrison Ford",
+    "Carrie Fischer",
+    "Någon annan skådis"
+  ]
+};
